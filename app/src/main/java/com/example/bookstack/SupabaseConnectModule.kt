71a0ea541f83320165fc5a5754431589b9be40da
tsuchida <yu41926t.tutida@icloud.com>
2026-01-11 00:36:55 +0900

    feat:Supabaseとの接続設定を行うModuleクラスを追加

---
+++ app/src/main/java/com/example/bookstack/SupabaseConnectModule.kt
@@ -0,0 +1,22 @@
+package com.example.bookstack
+
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.auth.Auth
+import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.serializer.KotlinXSerializer
+import kotlinx.serialization.json.Json
+
+object SupabaseConnectModule {
+    val client = createSupabaseClient(
+        supabaseUrl = BuildConfig.SUPABASE_URL,
+        supabaseKey = BuildConfig.SUPABASE_KEY
+    ) {
+        install(Postgrest) {
+            serializer = KotlinXSerializer(Json {
+                ignoreUnknownKeys = true
+            })
+        }
+
+        install(Auth)
+    }
+}
