c17644dcfb79182197ec33c649e41b9b2056c27f
tsuchida <yu41926t.tutida@icloud.com>
2026-01-12 00:44:52 +0900

    feat:匿名ログインを実装

--- app/src/main/java/com/example/bookstack/SupabaseConnectModule.kt
+++ app/src/main/java/com/example/bookstack/SupabaseConnectModule.kt
@@ -5,6 +5,7 @@ import io.github.jan.supabase.auth.Auth
 import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.serializer.KotlinXSerializer
 import kotlinx.serialization.json.Json
+import io.github.jan.supabase.auth.SettingsSessionManager
 
 object SupabaseConnectModule {
     val client = createSupabaseClient(
@@ -17,6 +18,10 @@ object SupabaseConnectModule {
             })
         }
         install(Auth) {
+            // トークンをどこに保存するかを管理するクラス
+            // 自動でSharedPreferences(Android標準の物理ストレージ)に保存
+            // v3系では 'storage' ではなく 'sessionManager' 変数を使用します
+            sessionManager = SettingsSessionManager()
             // 匿名認証を有効にする設定
             alwaysAutoRefresh = true
         }
